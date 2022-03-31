217603ffdb6674a06dceaa8688e1509eb325c299
PingPwn <102756204+pingpwn@users.noreply.github.com>
2022-03-31 21:13:50 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+import random
+
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('This shit works man, you can chill now ~ {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    dice = bool(random.getrandbits(1))
+
+    if dice = True:
+        if message.content.startswith('$hello'):
+        await message.channel.send('sup bitch')
+
+        if message.content.contains('$is the bot online','bot are you online','is bot online'):
+        await message.channel.send('yas')
+
+client.run('your token here')
