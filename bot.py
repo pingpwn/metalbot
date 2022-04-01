e88cf79e372e3536192b31d01a1d7ec41cf10ecd
PingPwn <102756204+pingpwn@users.noreply.github.com>
2022-04-01 14:11:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,16 +11,43 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+        
     if message.author == client.user:
-        return
+            return
+    elif message.author != client.user:
+        dice = bool(random.getrandbits(1))
 
-    dice = bool(random.getrandbits(1))
+        if "hello bot" in message.content.lower():
+                await message.channel.send('sup bitch')
 
-    if dice = True:
-        if message.content.startswith('$hello'):
-        await message.channel.send('sup bitch')
+        if "is the bot online" in message.content.lower():
+                await message.channel.send('yas')
+        if "bot lets have sex" in message.content.lower():
+                await message.channel.send('**moans**')
+        if "bot" and "some d" in message.content.lower():
+                await message.channel.send('B===D--')
+        if "who's right" in message.content.lower():
+            member = random.choice(message.guild.members)
+            await message.channel.send(member.mention + ' is right')
+       
+        if "bot" and "good metal artist" in message.content.lower():
+            await message.channel.send('idk, i\'d pick ' + random.choice(open('artists.txt').readlines()))
+        if "how does a computer work" in message.content.lower():
+            await message.channel.send('computer go brrr')
 
-        if message.content.contains('$is the bot online','bot are you online','is bot online'):
-        await message.channel.send('yas')
+        if "bot work" in message.content.lower():
+            await message.channel.send('shh... it\'s a secret ;\)')
+
+        if "bot" and "trans people" in message.content.lower():
+            await message.channel.send('Trans woman \( noun \): A man who was so upset about his small penis he decided he was gonna be bullied less by switching genders')
+
+        if dice == True:
+           if "hello bot" in message.content.lower():
+               await message.channel.send('sup bitch')
+        else:
+            if "hello bot" in message.content.lower():
+                await message.channel.send('fuck u')
+            else:
+                return
 
 client.run('your token goes here')
